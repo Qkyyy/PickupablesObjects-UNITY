991c7ffb2e914ed39681c474eef1111d9cb5bbdd
Łukasz <36705519+Qkyyy@users.noreply.github.com>
2018-11-10 12:41:31 +0100

    no message

---
+++ Assets/Scripts/PickupableObject.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupableObject : MonoBehaviour {
+
+    Transform playerTransform;          //player's Transform component  
+    Rigidbody rb;                       //object's rigidbody
+        
+    float throwPower = 10f;             //amount of throw power
+    bool pickedUp = false;              //bools which tells us if the objects was picked up
+
+    private void Awake()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        rb = transform.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        HandleObjectThrow();
+        HandleObjectPut();
+    }
+
+
+    //Picks up the object from ground
+    private void OnMouseDown()
+    {
+        if (Vector3.Distance(playerTransform.position, transform.position) < 2.2f)      //If player is close enough
+        {
+            transform.SetParent(playerTransform);                                       //sets the parent of the object to be player
+            rb.isKinematic = true;                                                      //turns Kinematic on, so object will not interfere when picked up
+            transform.localPosition = new Vector3(0f, 0.2f, 1.2f);                      //sets local position in front of player, so it seems player is holding it
+            transform.localRotation = Quaternion.Euler(0, 0f, 0);                       //rotates the object
+            pickedUp = true;                                                            //tell's that object was picked up
+        }
+    }
+
+    //Handles object throw
+    void HandleObjectThrow()
+    {
+            if (Input.GetMouseButtonDown(1) && pickedUp == true)                        //If player clicked RMB and object was picked up
+            {
+
+                transform.SetParent(null);                                              //unleashes object from its parent
+                rb.isKinematic = false;                                                 //turns off kinematic, so object can be physical again
+                rb.AddForce(new Vector3(Camera.main.transform.forward.x,0.2f, Camera.main.transform.forward.z) * throwPower, ForceMode.Impulse);    //Adds force in direction where camera is looking with 'throwpower' power and Impulse forcemode
+                pickedUp = false;                                                       //object is not picked up
+
+            }
+    }
+
+
+    //Handles putting object lightly on the ground
+    void HandleObjectPut()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && pickedUp == true)                            //if player clicked 'E' and picked up object before
+        {
+            transform.SetParent(null);                                                  //unleashes object from its parent
+            rb.isKinematic = false;                                                     //turns kinematic off, so object can lightly fall on the ground
+            pickedUp = false;                                                           //object is not picked up
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float playerSpeed = 400f;
+    public float mouseSensitivity = 1f;
+
+
+    float mouseUpDown = 0.0f;
+    float mouseRangeUpDown = 90f;
+
+    CharacterController characterController;
+
+    private void Start()
+    {
+        characterController = transform.GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        HandleMovement();
+        HandleMouseLook();
+    }
+
+    void HandleMovement()
+    {
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * playerSpeed;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * playerSpeed;
+
+        Vector3 move = new Vector3(x, 0, z);
+        move = transform.rotation * move;
+        characterController.Move(move * Time.deltaTime);
+    }
+
+    void HandleMouseLook()
+    {
+        float mouseLeftRight = Input.GetAxis("Mouse X") * mouseSensitivity;
+        transform.Rotate(0, mouseLeftRight, 0);
+
+        mouseUpDown -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        mouseUpDown = Mathf.Clamp(mouseUpDown, -mouseRangeUpDown, mouseRangeUpDown);
+        Camera.main.transform.localRotation = Quaternion.Euler(mouseUpDown, 0, 0);
+    }
+
+}
